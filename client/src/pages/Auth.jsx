59035bb2c1cd9f5e683bfce99a03b732595cf29e
krishna <krish77zalavadiya@gmail.com>
2026-03-11 19:48:34 +0530

    Initial commit

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+
+function Auth() {
+    return (
+        <div>
+        Auth Page
+        </div>
+    );
+}
+export default Auth
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+
+function Home() {
+    return (
+        <div>   
+            Home Page
+        </div>
+    );
+}
+export default Home;
